7ee56ae72b10cecc1fe2fbe51851f1b3c5918835
victorfleury <victor.fleury@gmail.com>
2025-05-07 23:00:37 -0400

    wip

--- internal/pkg/ui/tui.go
+++ internal/pkg/ui/tui.go
@@ -25,6 +25,7 @@ type Styles struct {
 	BorderColor lipgloss.Color
 	BorderStyle lipgloss.Style
 	BoldText    lipgloss.Style
+	Underlined  lipgloss.Style
 }
 
 func DefaultStyle(width int) *Styles {
@@ -32,6 +33,7 @@ func DefaultStyle(width int) *Styles {
 	s.BorderColor = lipgloss.Color("#e28743")
 	s.BorderStyle = lipgloss.NewStyle().Border(lipgloss.RoundedBorder()).BorderForeground(s.BorderColor).Padding(0, 1)
 	s.BoldText = lipgloss.NewStyle().Bold(true)
+	s.Underlined = lipgloss.NewStyle().Underline(true)
 	return s
 }
 
@@ -69,6 +71,7 @@ func (r RootModel) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 
 func (r RootModel) View() string {
 	title, ok := r.data["title"].(string)
+	comment, _ := r.data["comment"].(string)
 	if !ok {
 		title = "No job title ..."
 	}
@@ -77,8 +80,13 @@ func (r RootModel) View() string {
 		Align(lipgloss.Center, lipgloss.Center).
 		Render(r.style.BoldText.Render("🚜 GO Tractor ! 🚜"))
 	jobData := r.style.BorderStyle.Width(r.width-2).
-		Align(lipgloss.Center, lipgloss.Center).
-		Render(title)
+		Align(lipgloss.Left, lipgloss.Center).
+		Render(
+			r.style.Underlined.Render("Job title:"),
+			title,
+			r.style.Underlined.Render("\nComment   :"),
+			comment,
+		)
 	return lipgloss.Place(r.width, r.height, lipgloss.Center, lipgloss.Top, lipgloss.JoinVertical(lipgloss.Top, header, jobData))
 }
 
